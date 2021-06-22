64a41b4e35a9a3d09a4e7a96d14087f93f8f762f
Michael Giesen <michael@unvetica.com>
2021-06-22 09:56:55 -0700

    official ECH website files

---
+++ js/script2.js
@@ -0,0 +1,34 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '315',
+    width: '560',
+    //LIVE STREAM ID: GQcXUn5XG5c
+    //NORMAL videoId: 'qnu_tBM9aGM',
+    videoId: 'BJ7X3csL9vc',
+    events: {
+      'onReady': function() {
+        $(".video-thumb").click(function() {
+          var $this = $(this);
+          if (!$this.hasClass("active")) {
+            player.loadVideoById($this.attr("data-video"));
+                        $this.addClass("active").siblings().removeClass("active");
+          }
+        });
+
+   
+
+      }
+    }
+  });
+}
+
+
+
+
+
---
+++ js/script3.js
@@ -0,0 +1,24 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '315',
+    width: '560',
+    videoId: 'qnu_tBM9aGM',
+    events: {
+      'onReady': function() {
+        $(".video-thumb").click(function() {
+          var $this = $(this);
+          if (!$this.hasClass("active")) {
+            player.loadVideoById($this.attr("data-video"));
+                        $this.addClass("active").siblings().removeClass("active");
+          }
+        });
+      }
+    }
+  });
+}
