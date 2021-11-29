43b6a231d1ebf24188a0162dfa9fd43a9aff7fdb
jerik.garcia <86265398+jerik-unvetica@users.noreply.github.com>
2021-11-29 19:35:46 +0800

    Add files via upload

--- js/script2.js
+++ js/script2.js
@@ -10,7 +10,7 @@ function onYouTubeIframeAPIReady() {
     width: '560',
     //LIVE STREAM ID: GQcXUn5XG5c
     //NORMAL videoId: 'qnu_tBM9aGM',
-    videoId: 'YzL2GmxA6Wk',
+    videoId: 'aH3Le-3v320',
     events: {
       'onReady': function() {
         $(".video-thumb").click(function() {
