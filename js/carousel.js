9e509c3173b069a375580eb9f227d1b7c531b5eb
Samarth-001 <samarthagl2002@gmail.com>
2023-01-30 00:49:47 +0530

    Route fix and carousel update

---
+++ js/carousel.js
@@ -0,0 +1,29 @@
+ let swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 4,
+        },
+    },
+  });
